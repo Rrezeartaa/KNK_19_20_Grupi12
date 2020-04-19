6c103edb2828c7828c326f8e894f831e9d2257d0
Rrezeartaa <56273238+Rrezeartaa@users.noreply.github.com>
2020-04-19 19:10:56 +0200

    Create Hyrja.java

---
+++ LogindheSignUp/Hyrja.java
@@ -0,0 +1,21 @@
+package application;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+public class Hyrja extends Application {
+
+  @Override
+  public void start(Stage primaryStage) throws Exception {	  
+   
+	Parent parenti = FXMLLoader.load(getClass().getClassLoader().getResource("hyrja.fxml"));
+    Scene scene = new Scene(parenti);
+    primaryStage.setScene(scene);
+    primaryStage.show();
+  }
+  public static void main(String[] args) {
+	    Application.launch(args);
+	  }
+}
