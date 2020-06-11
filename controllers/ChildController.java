68c60c3a2b37bfdf742214b5c38af68f200733c4
vanesarama <56617704+vanesarama@users.noreply.github.com>
2020-06-11 23:13:08 +0200

    Update ChildController.java

--- controllers/ChildController.java
+++ controllers/ChildController.java
@@ -2,3 +2,7 @@ package pwm.controllers;
 
 public abstract class ChildController extends BaseController {
   public MainController parentController;
+    public void setParentController(MainController controller) {
+    this.parentController = controller;
+  }
+}
