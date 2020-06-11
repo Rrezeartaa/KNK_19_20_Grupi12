af3188cb4fcbe1323b26c5fc30ecc312dbdd30fc
Vlora Gjoka <58333814+VloraGjoka@users.noreply.github.com>
2020-06-11 22:46:23 +0200

    Create BaseController.java

---
+++ controllers/BaseController.java
@@ -0,0 +1,11 @@
+package pwm.controllers;
+import java.net.URL;
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+import javafx.fxml.Initializable;
+import pwm.utils.SessionManager;
+
+public abstract class BaseController implements Initializable {
+
+  @Override
