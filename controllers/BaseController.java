cbcb9a3ad0e7ce4a1cccb21b85ca976a06556efa
vanesarama <56617704+vanesarama@users.noreply.github.com>
2020-06-11 22:55:45 +0200

    Update BaseController.java

--- controllers/BaseController.java
+++ controllers/BaseController.java
@@ -8,4 +8,16 @@ import pwm.utils.SessionManager;
 
 public abstract class BaseController implements Initializable {
 
+  
   @Override
+    public void initialize(URL arg0, ResourceBundle arg1) {
+    loadLangTexts(getLangBundle());
+  }
+
+  public ResourceBundle getLangBundle() {
+    Locale locale = SessionManager.getLocale();
+    return ResourceBundle.getBundle("pwm.bundles.LangBundle", locale);
+  }
+
+  public abstract void loadLangTexts(ResourceBundle langBundle);
+}
