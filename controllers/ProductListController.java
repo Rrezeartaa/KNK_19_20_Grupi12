82d871920c712cd55a14d01ec9998c99d95b8208
Vlora Gjoka <58333814+VloraGjoka@users.noreply.github.com>
2020-06-12 16:14:01 +0200

    Update ProductListController.java

--- controllers/ProductListController.java
+++ controllers/ProductListController.java
@@ -27,3 +27,34 @@ import pwm.components.FindProductComponent;
 import pwm.components.PaginationComponent;
 import pwm.models.Product;
 import pwm.repositories.ProductRepository;
+public class ProductListController extends ChildController {
+  final KeyCombination keyCtrlF = new KeyCodeCombination(KeyCode.F, KeyCombination.CONTROL_ANY);
+  private final int PAGE_SIZE = 10;
+
+  private PaginationComponent paginationComponent;
+
+  @FXML
+  private TableView<Product> tableView;
+  @FXML
+  private TableColumn<Product, Integer> idColumn;
+  @FXML
+  private TableColumn<Product, String> titleColumn;
+  @FXML
+  private TableColumn<Product, Double> priceColumn;
+  @FXML
+  private TableColumn<Product, Double> qtyColumn;
+  @FXML
+  private ToggleButton multipleButton;
+  @FXML
+  private Button findButton;
+  @FXML
+  private Button showAllButton;
+  @FXML
+  private HBox paginationPane;
+  @FXML
+  private MenuItem viewMenuItem;
+  @FXML
+  private MenuItem editMenuItem;
+  @FXML
+  private MenuItem removeMenuItem;
+
