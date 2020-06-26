538bbdd9a82ee21a7ba98695338a141cb5336609
visarahmeti <58608274+Visskey@users.noreply.github.com>
2020-06-26 14:48:40 +0200

    Update slideshow.java

--- application/controllers/slideshow.java
+++ application/controllers/slideshow.java
@@ -15,5 +15,14 @@ import java.util.ResourceBundle;
 public class Controller implements Initializable {
     @FXML ImageView imageView;
     int count;
+    
+    public void slideshow(){
+        ArrayList<Image> images=new ArrayList<Image>();
+        images.add(new Image("/b2.jpg"));
+        images.add(new Image("/b3.jpg"));
+        images.add(new Image("/b4.jpg"));
+        images.add(new Image("/b1.jpg"));
+    
+    
   
 }
