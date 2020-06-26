54047b7c55fa838fda46345afbc8767812a9857d
visarahmeti <58608274+Visskey@users.noreply.github.com>
2020-06-26 14:47:17 +0200

    Update slideshow.java

--- application/controllers/slideshow.java
+++ application/controllers/slideshow.java
@@ -4,3 +4,10 @@ import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.util.Duration;
+
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.ResourceBundle;
