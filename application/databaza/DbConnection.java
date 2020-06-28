cb27c9181f7c5d9c3d8cf58ea2221b42e5b19e07
Lum Pireva <pirevalum@gmail.com>
2020-06-28 14:01:48 +0200

    Database connection
    Databaza e krijuar ne localhost/phpmyadmin.
    Emri i databazes eshte 'aeroporti'.

---
+++ application/databaza/DbConnection.java
@@ -0,0 +1,27 @@
+package application.databaza;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DbConnection {
+	private static Connection dbConnection;
+
+	private final static String host = "localhost";
+	private final static String dbName = "aeroporti";
+	private final static String username = "root";
+	private final static String password = "";
+
+	public static Connection getConnection() {
+		if (dbConnection == null) {
+			try {
+				Class.forName("com.mysql.jdbc.Driver");
+				dbConnection = DriverManager.getConnection(
+						"jdbc:mysql://" + host + "/" + dbName + "?useSSL=false&allowPublicKeyRetrieval=true", username,
+						password);
+			} catch (Exception ex) {
+				ex.printStackTrace();
+			}
+		}
+		return dbConnection;
+	}
+}
