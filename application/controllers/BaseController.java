8690eea95615bba8cc5a59ad068976817e2eee0d
Rrezeartaa <56273238+Rrezeartaa@users.noreply.github.com>
2020-06-29 17:30:15 +0200

    Create BaseController.java

---
+++ application/controllers/BaseController.java
@@ -0,0 +1,11 @@
+package application.controllers;
+
+import java.net.URL;
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+import javafx.fxml.Initializable;
+import application.controllers.slideshow;
+
+public abstract class BaseController implements Initializable {
+}
