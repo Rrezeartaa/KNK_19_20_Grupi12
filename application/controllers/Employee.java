5e74e506ee016a494be6855c01d7fde547deff39
Vlora Gjoka <58333814+VloraGjoka@users.noreply.github.com>
2020-06-29 13:50:34 +0200

    Create Employee.java

---
+++ application/controllers/Employee.java
@@ -0,0 +1,15 @@
+package javafxtableviewaddrows;
+
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+
+/**
+ *
+ * @author Cool IT help
+ */
+public class Employee {    
+    private  final SimpleStringProperty Airline;
+    private  final SimpleStringProperty From;
+    private  final SimpleStringProperty Date;
+    private  final SimpleStringProperty Scheduled;
+    private  final SimpleIntegerProperty Status;
