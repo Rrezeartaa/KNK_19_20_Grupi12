692ba9618eaf19930734ae7377af1fbe41fb8d26
zahirmaliqi <58596989+zahirmaliqi@users.noreply.github.com>
2020-06-29 20:54:57 +0200

    Dritarja opsionale e covid19

---
+++ application/covvideo.java
@@ -0,0 +1,68 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Region;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.scene.media.MediaView;
+
+public class halla extends Application {
+   
+
+    @Override
+    public void start(Stage primaryStage) {
+        MediaPlayer mediaPlayer = new MediaPlayer(new Media(TEST_URL));
+        MediaView mediaView = new MediaView(mediaPlayer);
+
+        Button playButton = new Button("Play");
+        playButton.setOnAction(e -> {
+            if (playButton.getText().equals("Play")) {
+                mediaPlayer.play();
+                playButton.setText("Pause");
+            } else {
+                mediaPlayer.pause();
+                playButton.setText("Play");
+            }
+        });
+
+        Button fullScreen = new Button("[[]]");
+        fullScreen.setOnAction(e -> {
+            if (fullScreen.getText().equals("[[]]")) {
+                ((Stage)mediaView.getScene().getWindow()).setFullScreen(true);
+                fullScreen.setText("]][[");
+            } else {
+                ((Stage)mediaView.getScene().getWindow()).setFullScreen(false);
+                fullScreen.setText("[[]]");
+            }
+        });
+
+        Slider slVolume = new Slider();
+        slVolume.setPrefWidth(180);
+        slVolume.setMaxWidth(Region.USE_PREF_SIZE);
+        slVolume.setMinWidth(30);
+        slVolume.setValue(60);
+        mediaPlayer.volumeProperty().bind(slVolume.valueProperty().divide(100));
+
+        HBox hBox = new HBox(10);
+        hBox.setAlignment(Pos.CENTER);
+        hBox.getChildren().addAll(playButton, fullScreen,
+                new Label("Volume"), slVolume);
+
+        BorderPane pane = new BorderPane();
+        pane.setCenter(mediaView);
+        pane.setBottom(hBox);
+
+        Scene scene = new Scene(pane, 800, 600);
+        primaryStage.setTitle("Media Player Demo");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+}
