2c218828c6565a139216fe42ceb82fc72e8497de
Rrezeartaa <56273238+Rrezeartaa@users.noreply.github.com>
2020-06-29 17:23:25 +0200

    Editim

--- application/controllers/FXMLDocumentController.java
+++ application/controllers/FXMLDocumentController.java
@@ -39,11 +39,18 @@ public class FXMLDocumentController implements Initializable {
         Date.setCellValueFactory(new PropertyValueFactory<>("Date"));        
         Scheduled.setCellValueFactory(new PropertyValueFactory<>("Scheduled"));        
         Status.setCellValueFactory(new PropertyValueFactory<>("Status"));       
-        
-        
+               
         Employee emp1 = new Employee("CHAIR Airlines", "Zurich", "28 Jun", "08:05", "Arrived 08:22");
-        Employee emp2 = new Employee( "EDELWEISS Air", "Zurich", "26 Jun", "08:15", "Arrive 08:38");
-        
+        Employee emp2 = new Employee( "EDELWEISS Air", "Zurich", "26 Jun", "08:15", "Arrived 08:38");
+        Employee emp3 = new Employee( "TURKISH Airlines", "Instambul", "3 July", "08:20", "Cancelled");
+        Employee emp4 = new Employee("EASYJET Europe", "Berlin", "04 July", "08:35", "Cancelled"); 
+        Employee emp5 = new Employee("EUROWINGS ", "Bern", "30 Jun", "08:05", "Arrived 08:22");
+        Employee emp6 = new Employee( "AIR MEDITERRAEN", "Stuttgart", "01 July", "10:15", "Arrive 11:38");
+        Employee emp7 = new Employee( "AURTIAL Airlines", "Vienna", "3 July", "08:20", "Cancelled");
+        Employee emp8 = new Employee("SWISS", "Oslo", "05 July", "11:35", "Cancelled");  
+           
+        dataList.addAll(emp1,emp2, emp3, emp4,emp5,emp6,emp7,emp8);
+            FilteredList<Employee> filteredData = new FilteredList<>(dataList, b -> true);
        
 		filterField.textProperty().addListener((observable, oldValue, newValue) -> {
 			filteredData.setPredicate(employee -> {
@@ -69,6 +76,16 @@ public class FXMLDocumentController implements Initializable {
 		sortedData.comparatorProperty().bind(tableview.comparatorProperty());
 		
 		tableview.setItems(sortedData);
+	    				
+         }    
+    @FXML
+	  private void home(ActionEvent event) throws Exception {
 	    
-				
-        
+		  Parent parenti = FXMLLoader.load(getClass().getClassLoader().getResource("application/views/sample.fxml"));
+	      Scene scene = new Scene(parenti);
+	      Stage primaryStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+	      primaryStage.setScene(scene);
+	      primaryStage.show();
+	    
+	  }
+}
