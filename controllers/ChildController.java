3f7ea2ce1a121118d6ccb876db4dd5e533f23431
Vlora Gjoka <58333814+VloraGjoka@users.noreply.github.com>
2020-06-30 13:15:05 +0200

    Delete ChildController.java

--- controllers/ChildController.java
+++
@@ -1,8 +0,0 @@
-package pwm.controllers;
-
-public abstract class ChildController extends BaseController {
-  public MainController parentController;
-    public void setParentController(MainController controller) {
-    this.parentController = controller;
-  }
-}
